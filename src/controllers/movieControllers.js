0aadb0244475219dcccbde3809107deedb4ec6ef
Florian <f.schaessens@gmail.com>
2024-02-12 21:51:18 +0100

    Express 01.2 - 🚀 Lecture depuis la base de données

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -25,23 +25,75 @@ const movies = [
   },
 ];
 
+const database = require("../../database");
+
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies); // use res.json instead of console.log
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movies]) => {
+      if (movies[0] != null) {
+        res.json(movies[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+const getUsers = (req, res) => {
+  database
+    .query("SELECT * FROM users")
+    .then(([result]) => {
+      res.status(200).json({
+        status: 200,
+        message: "Success",
+        users: result,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getUsersID = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([result]) => {
+      if (result[0] != null) {
+        res.json(result[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 module.exports = {
   getMovies,
   getMovieById,
+  getUsers,
+  getUsersID,
 };
