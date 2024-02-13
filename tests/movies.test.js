194ae9bc541b9a06605fbcf93b346ec6fa7a7067
Florian <f.schaessens@gmail.com>
2024-02-13 16:37:21 +0100

    Express 02.1 - 🧪 Tester les routes POST

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const app = require("../src/app");
 
 const database = require("../database");
+const crypto = require("node:crypto");
 
 afterAll(() => database.end());
 
@@ -29,5 +30,85 @@ describe("GET /api/movies/:id", () => {
     const response = await request(app).get("/api/movies/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+
+describe("POST /api/movies", () => {
+  it("should return created movie", async () => {
+    const newMovie = {
+      title: "Star Wars",
+      director: "George Lucas",
+      year: "1977",
+      color: "1",
+      duration: 120,
+    };
+
+    const response = await request(app).post("/api/movies").send(newMovie);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM movies WHERE id=?",
+      response.body.id
+    );
+
+    const [movieInDatabase] = result;
+
+    Object.keys(newMovie).forEach((key) => {
+      expect(movieInDatabase).toHaveProperty(key);
+      expect(movieInDatabase[key]).toEqual(newMovie[key]);
+    });
+  });
+
+  it("should return an error", async () => {
+    const movieWithMissingProps = { title: "Harry Potter" };
+
+    const response = await request(app)
+      .post("/api/movies")
+      .send(movieWithMissingProps);
+
+    expect(response.status).toEqual(500);
+  });
+});
+
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "Marie",
+      lastname: "Martin",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "Paris",
+      language: "French",
+    };
+
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+
+    const [usersDatabase] = result;
+
+    Object.keys(newUser).forEach((key) => {
+      expect(usersDatabase).toHaveProperty(key);
+      expect(usersDatabase[key]).toEqual(newUser[key]);
+    });
+  });
+
+  it("should return an error", async () => {
+    const userWithMissingProps = { firstname: "Marie" };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(userWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
