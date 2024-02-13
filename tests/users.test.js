f0af5dc89556127ee717c876a77f20af808613c6
Florian <f.schaessens@gmail.com>
2024-02-13 22:53:56 +0100

    Express 02.1 - 🧪 Tester les routes POST

--- tests/users.test.js
+++ tests/users.test.js
@@ -31,3 +31,43 @@ describe("GET /api/users/:id", () => {
     expect(response.status).toEqual(404);
   });
 });
+
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "Marie",
+      lastname: "Martin",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "Paris",
+      language: "French",
+    };
+
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+
+    const [usersDatabase] = result;
+
+    Object.keys(newUser).forEach((key) => {
+      expect(usersDatabase).toHaveProperty(key);
+      expect(usersDatabase[key]).toEqual(newUser[key]);
+    });
+  });
+
+  it("should return an error", async () => {
+    const userWithMissingProps = { firstname: "Marie" };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(userWithMissingProps);
+
+    expect(response.status).toEqual(500);
+  });
+});
