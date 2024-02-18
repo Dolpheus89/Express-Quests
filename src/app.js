16b24c57151b0038488c3c65b558162872d7ce56
Florian <f.schaessens@gmail.com>
2024-02-18 21:30:48 +0100

    Express 04 - 🗑️ Méthode DELETE et suppression de données

--- src/app.js
+++ src/app.js
@@ -5,18 +5,22 @@ const app = express();
 app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
+const userControllers = require("./controllers/userControllers");
 const validateMovie = require("./middlewares/validateMovie");
 const validateUser = require("./middlewares/validateUsers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
-app.get("/api/users", movieControllers.getUsers);
-app.get("/api/users/:id", movieControllers.getUsersID);
+app.get("/api/users", userControllers.getUsers);
+app.get("/api/users/:id", userControllers.getUsersID);
 
 app.post("/api/movies", validateMovie, movieControllers.postMovie);
-app.post("/api/users", validateUser, movieControllers.postUser);
+app.post("/api/users", validateUser, userControllers.postUser);
 
 app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
-app.put("/api/users/:id", validateUser, movieControllers.updateUser);
+app.put("/api/users/:id", validateUser, userControllers.updateUser);
+
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
+app.delete("/api/users/:id", userControllers.deleteUser);
 
 module.exports = app;
